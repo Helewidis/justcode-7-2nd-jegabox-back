b79e79538a7e68ebe9625a21c418a319dd1cf667
Helewidis <108878708+Helewidis@users.noreply.github.com>
2022-11-23 22:14:05 +0900

    for pull dev

--- routes/movieRouter.js
+++ routes/movieRouter.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const controller = require('../controllers/movieController');
 
 router.get('/main', controller.getMainMovies);
-router.get('/list', controller.getAllMovies);
+router.post('/list', controller.getAllMovies);
 router.get('/comingsoon', controller.getComingsoonMovies);
 router.post('/', controller.searchTitle);
 router.get('/', controller.searchText);
